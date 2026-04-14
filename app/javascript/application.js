d666bcbe2e5ef0d969fbd53e90dfff62dc91139e
Roddy <r.vellayoudom@hookto.fr>
2026-04-14 16:03:39 +0200

    add button to start content analysis

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,28 +4,43 @@ import "controllers"
 
 import { imageAnalysis } from './mediapipe/image_analysis/image_analysis.js'
 import { videoAnalysis } from './mediapipe/video_analysis/video_analysis.js'
- 
+
 const image = document.getElementById('image_input')
 const video = document.getElementById('video_input')
 
 const canvas = document.getElementById("canvas");
 const fileUploader = document.querySelector('.file-uploader')
 
-fileUploader.addEventListener('change', function () { 
+const button = document.getElementById('btn-analysis')
+let fileType = null;
+
+fileUploader.addEventListener('change', function () {
+  console.log('file uploaded')
   const file = this.files[0];
   if (!file) return
+
+  button.style.display = '' 
+  fileUploader.style.display = 'none'
 
   const url = URL.createObjectURL(file)
 
   if (file.type.startsWith('image/')) {
+    fileType = 'image'
     image.src = url
     image.style.display = 'block'
   }
     else if (file.type.startsWith('video/')) {
+    fileType = 'video'
     video.src = url
     video.style.display = 'block'
   }
 })
 
-image.addEventListener('load', function() { imageAnalysis(image, canvas) })
-video.addEventListener('loadedmetadata', function() { videoAnalysis(video, canvas) })
+button.addEventListener("click", ()=> {
+  if (fileType === 'image') {
+    imageAnalysis(image, canvas)
+  } 
+  else if (fileType === 'video') {
+    videoAnalysis(video, canvas)
+  }
+});
